4557005e6ccbe16728c6a2b6d545de0feb55b58a
sergiopeluzzi <sergiopeluzzi@gmail.com>
2016-02-26 17:34:18 -0400

    Ajustes no logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -71,10 +71,4 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
-/*
-    public function getLogout(){
-        Auth::logout();
-        Session::flush();
-        return Redirect::to('/');
-    }*/
 }
